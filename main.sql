5b589ba9494ef3077fc165c23ca0c9b768d309a7
MartinAchterman <65299501+roadpilot@users.noreply.github.com>
2021-07-13 04:20:44 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,2 @@
+select concat(name,'(',left(occupation,1),')') from occupations order by name;
+select concat('There are a total of ',count(*),' ',lcase(occupation),'s.') from occupations group by occupation order by count(*), occupation;
